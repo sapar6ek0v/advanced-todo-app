496233775e5f57f4d9cd99f49da39ccc3d811604
sapar6ek0v <razetka0309@gmail.com>
2022-11-17 11:46:33 +0300

    set up firebase

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { FC, useEffect } from 'react';
+import { collection, getDocs } from 'firebase/firestore';
+import { db } from './utils/firebaseConfig';
 
-function App() {
+const App: FC = () => {
+  const todoCollectionRef = collection(db, 'todo');
+
+  const getTodos = async () => {
+    const data = await getDocs(todoCollectionRef);
+    console.log(data)
+  };
+
+  useEffect(() => {
+    getTodos()
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Edit <code>src/App.tsx</code> and save to reload.
     </div>
   );
 }
